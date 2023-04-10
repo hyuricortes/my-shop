485571609db7648e4864810d6de352d2699a9302
Hyuri.Cortes <hyuri@hldesenvolvimentoweb.com.br>
2023-04-10 18:08:31 -0300

    feat: pages

---
+++ src/pages/product/[product_id].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function Product() {
+    const { query } = useRouter()
+
+    return (
+        <h1>Produto { query.product_id }</h1>
+    )
+}
